aeec98f55472fc94bb8f69674cef58e109faf565
Taylor Fahlman <tfahlman@redhat.com>
2020-04-11 13:45:21 -0700

    Make database conn held in package. Create functions for getting all Games and one Game

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-    
-    "log"
+	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	mydb "github.com/fahlmant/backlog-api/pkg/database"
 	"github.com/fahlmant/backlog-api/pkg/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	router := mux.NewRouter()
-	
+
 	handlers.HandleRequests(router)
-	
+
+	mydb.InitDb()
+	defer mydb.DB.Close()
+
 	http.Handle("/", router)
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,50 +1,130 @@
 package database
 
-func InitDb() {
-    config := dbConfig()
-    var err error
-    psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-        "password=%s dbname=%s sslmode=disable",
-        config[dbhost], config[dbport],
-        config[dbuser], config[dbpass], config[dbname])
+import (
+	"database/sql"
+	"fmt"
+	"os"
 
-    db, err = sql.Open("postgres", psqlInfo)
-    if err != nil {
-        panic(err)
-    }
-    err = db.Ping()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Successfully connected!")
+	_ "github.com/lib/pq"
+	"github.com/google/uuid"
+
+	"github.com/fahlmant/backlog-api/pkg/types"
+)
+
+const (
+    dbhost = "DBHOST"
+    dbport = "DBPORT"
+    dbuser = "DBUSER"
+    dbpass = "DBPASS"
+    dbname = "DBNAME"
+)
+
+var (
+	DB *sql.DB
+)
+
+
+func InitDb() {
+	var err error
+	config := dbConfig()
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		config[dbhost], config[dbport],
+		config[dbuser], config[dbpass], config[dbname])
+
+	DB, err = sql.Open("postgres",psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println("Successfully connected!") 
 }
 
-func DBConfig() map[string]string {
-    conf := make(map[string]string)
-    host, ok := os.LookupEnv(dbhost)
-    if !ok {
-        panic("DBHOST environment variable required but not set")
-    }
-    port, ok := os.LookupEnv(dbport)
-    if !ok {
-        panic("DBPORT environment variable required but not set")
-    }
-    user, ok := os.LookupEnv(dbuser)
-    if !ok {
-        panic("DBUSER environment variable required but not set")
-    }
-    password, ok := os.LookupEnv(dbpass)
-    if !ok {
-        panic("DBPASS environment variable required but not set")
-    }
-    name, ok := os.LookupEnv(dbname)
-    if !ok {
-        panic("DBNAME environment variable required but not set")
-    }
-    conf[dbhost] = host
-    conf[dbport] = port
-    conf[dbuser] = user
-    conf[dbpass] = password
-    conf[dbname] = name
-    return conf
+func dbConfig() map[string]string {
+	conf := make(map[string]string)
+	host, ok := os.LookupEnv(dbhost)
+	if !ok {
+		panic("DBHOST environment variable required but not set")
+	}
+	port, ok := os.LookupEnv(dbport)
+	if !ok {
+		panic("DBPORT environment variable required but not set")
+	}
+	user, ok := os.LookupEnv(dbuser)
+	if !ok {
+		panic("DBUSER environment variable required but not set")
+	}
+	password, ok := os.LookupEnv(dbpass)
+	if !ok {
+		panic("DBPASS environment variable required but not set")
+	}
+	name, ok := os.LookupEnv(dbname)
+	if !ok {
+		panic("DBNAME environment variable required but not set")
+	}
+	conf[dbhost] = host
+	conf[dbport] = port
+	conf[dbuser] = user
+	conf[dbpass] = password
+	conf[dbname] = name
+	return conf
+}
+
+func GetAllGames(db *sql.DB) ([]types.Game, error) {
+
+	var games []types.Game
+	rows, err := db.Query(`
+	SELECT
+		id,
+		title,
+		platform
+	FROM games`)
+
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		game := types.Game{}
+		err = rows.Scan(
+			&game.ID,
+			&game.Title,
+			&game.Platform,
+		)
+		if err != nil {
+			return nil, err
+		}
+		games = append(games, game)
+	}
+
+	return games, nil
+}
+
+func GetGame(db *sql.DB, uuid uuid.UUID) (types.Game, error) {
+
+	var game types.Game
+
+	sqlStatement := `SELECT * FROM games WHERE id=$1`
+	row := db.QueryRow(sqlStatement, uuid)
+	err := row.Scan(
+		&game.ID,
+		&game.Title,
+		&game.Platform,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Zero rows found")
+		} else {
+			fmt.Printf("%+v\n", err)
+			return types.Game{}, err
+		}
+	}
+
+	return game, nil
+
 }
