d4c15cdfcb96453332c752af45ab694d60828d9a
Taylor Fahlman <tfahlman@redhat.com>
2020-11-08 09:29:28 -0500

    Implement PATCH for games

--- pkg/handlers/games.go
+++ pkg/handlers/games.go
@@ -17,6 +17,7 @@ func registerGameHandlers(router *mux.Router) {
 	router.HandleFunc("/games", gamePost).Methods("POST")
 	router.HandleFunc("/games/{id}", gameGet).Methods("GET")
 	router.HandleFunc("/games/{id}", gamePut).Methods("PUT")
+	router.HandleFunc("/games/{id}", gamePatch).Methods("PATCH")
 	router.HandleFunc("/games/{id}", gameDelete).Methods("DELETE")
 }
 
@@ -107,6 +108,31 @@ func gamePut(w http.ResponseWriter, r *http.Request) {
 
 	//Update the database entry
 	mydb.UpdateGame(mydb.DB, game)
+}
+
+func gamePatch(w http.ResponseWriter, r *http.Request) {
+
+	//Delcare types needed
+	var game types.Game
+	//var err error
+
+	//Retrieve the object ID from the request and store it in the game struct
+	id := mux.Vars(r)["id"]
+
+	game, _ = mydb.GetGame(mydb.DB, uuid.MustParse(id))
+
+	//Get the rest of the game object data from the request body
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &game)
+
+	if game.Title == "" || game.Platform == "" {
+		json.NewEncoder(w).Encode(struct{ Error string }{"Missing Title or Platform"})
+		return
+	}
+
+	//Update the database entry
+	mydb.UpdateGame(mydb.DB, game)
+
 }
 
 func gameDelete(w http.ResponseWriter, r *http.Request) {
