e936c99db2d5c3be830b87b42308b901ae4fe47b
jordan-lange25 <jordan.lange03@gmail.com>
2022-07-25 09:46:26 -0700

    allow read of config file

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -3,31 +3,22 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
-
+	"github.com/spf13/viper"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbhost    = "DBHOST"
-	dbport    = "DBPORT"
-	dbuser    = "DBUSER"
-	dbpass    = "DBPASS"
-	dbname    = "DBNAME"
-	gameTable = `games`
-)
 
 var (
 	DB *sql.DB
 )
 
-func InitDb() {
+func dbInit() {
 	var err error
 	config := dbConfig()
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		config[dbhost], config[dbport],
-		config[dbuser], config[dbpass], config[dbname])
+		config["dbhost"], config["dbport"],
+		config["dbuser"], config["dbpass"], config["dbname"])
 
 	DB, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -42,31 +33,31 @@ func InitDb() {
 }
 
 func dbConfig() map[string]string {
+	viper.AddConfigPath("/Users/jordanlange/Documents/projects/personal/backlog-api/pkg/config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+	
 	conf := make(map[string]string)
-	host, ok := os.LookupEnv(dbhost)
-	if !ok {
-		panic("DBHOST environment variable required but not set")
-	}
-	port, ok := os.LookupEnv(dbport)
-	if !ok {
-		panic("DBPORT environment variable required but not set")
-	}
-	user, ok := os.LookupEnv(dbuser)
-	if !ok {
-		panic("DBUSER environment variable required but not set")
-	}
-	password, ok := os.LookupEnv(dbpass)
-	if !ok {
-		panic("DBPASS environment variable required but not set")
-	}
-	name, ok := os.LookupEnv(dbname)
-	if !ok {
-		panic("DBNAME environment variable required but not set")
-	}
-	conf[dbhost] = host
-	conf[dbport] = port
-	conf[dbuser] = user
-	conf[dbpass] = password
-	conf[dbname] = name
+	host := viper.GetString("database.dbhost")
+	fmt.Println(host)
+	port := viper.GetString("database.dbport")
+	fmt.Println(port)
+	user := viper.GetString("database.dbuser")
+	fmt.Println(user)
+	password := viper.GetString("database.dbpass")
+	fmt.Println(password)
+	name := viper.GetString("database.dbname")
+	fmt.Println(name)
+
+	conf["dbhost"] = host
+	conf["dbport"] = port
+	conf["dbuser"] = user
+	conf["dbpass"] = password
+	conf["dbname"] = name
+	fmt.Println(conf)
 	return conf
-}
+	}
